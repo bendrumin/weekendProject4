dab25a85f77730b4a0843b6ac084b7732a06b2b8
bendrumin <bendrumin@mac.com>
2017-07-14 21:51:29 -0500

    bringing in images

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,4 +5,12 @@ myApp.controller('WelcomeController', function(){
 
   var welcome = this;
   welcome.message = 'Hello, welcome to my life';
+
+  welcome.names = ['images/imageOne.jpg',
+  'images/imageTwo.jpg',
+  'images/imageThree.jpg',
+  'images/imageFour.jpg',
+  'images/imageFive.jpg',
+  'images/imageSix.jpg'];
+
 });
