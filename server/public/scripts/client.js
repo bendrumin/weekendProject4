0bc815f3bb6903bae5cc42868d00488495861716
bendrumin <bendrumin@mac.com>
2017-07-15 12:44:27 -0500

    Adding the toggleShow but the description always shows

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,12 +5,56 @@ myApp.controller('WelcomeController', function(){
 
   var welcome = this;
   welcome.message = 'Hello, welcome to my life';
+  welcome.photos = [];
+  createphotos();
 
-  welcome.names = ['images/imageOne.jpg',
-  'images/imageTwo.jpg',
-  'images/imageThree.jpg',
-  'images/imageFour.jpg',
-  'images/imageFive.jpg',
-  'images/imageSix.jpg'];
+  welcome.toggleShow = function(index){
+    welcome.photos[index].visible = !welcome.photos[index].visible;
+    console.log(welcome.photos[index].visible);
+  };
+
+  welcome.toggleComments= function(index){
+    welcome.photos[index].commentsVisible = !welcome.photos[index].commentsVisible;
+    console.log(welcome.photos[index].visible);
+  };
+
+  function createphotos(){
+    welcome.photos[0] = {
+      pic: "/images/imageOne.jpg",
+      desc: "Photo description",
+      likes: 0,
+      visible: true,
+    };
+    welcome.photos[1] = {
+      pic: "images/imageTwo.jpg",
+      desc: "Photo description",
+      likes: 0,
+      visible: true,
+    };
+    welcome.photos[2] = {
+      pic: "images/imageThree.jpg",
+      desc: "Photo description",
+      likes: 0,
+      visible: true,
+    };
+    welcome.photos[3] = {
+      pic: "images/imageFour.jpg",
+      desc: "Photo description",
+      likes: 0,
+      visible: true,
+    };
+    welcome.photos[4] = {
+      pic: "images/imageFive.jpg",
+      desc: "Photo description",
+      likes: 0,
+      visible: true,
+    };
+    welcome.photos[5] = {
+      pic: "images/imageSix.jpg",
+      desc: "Photo description",
+      likes: 0,
+      visible: true,
+    };
+  }
 
 });
