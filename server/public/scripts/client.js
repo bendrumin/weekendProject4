f4997ca94007bdbceec5884e9727610b0625c29a
bendrumin <bendrumin@mac.com>
2017-07-15 14:43:28 -0500

    Images now show up, adding some styles

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,50 +8,50 @@ myApp.controller('WelcomeController', function(){
   welcome.photos = [];
   createphotos();
 
+  welcome.incrementLike = function(index){
+   welcome.photos[index].likes += 1;
+ };
+
   welcome.toggleShow = function(index){
     welcome.photos[index].visible = !welcome.photos[index].visible;
-    console.log(welcome.photos[index].visible);
   };
 
-  welcome.toggleComments= function(index){
-    welcome.photos[index].commentsVisible = !welcome.photos[index].commentsVisible;
-    console.log(welcome.photos[index].visible);
-  };
+
 
   function createphotos(){
     welcome.photos[0] = {
-      pic: "/images/imageOne.jpg",
-      desc: "Photo description",
+      pic: "/images/image - 1.jpg",
+      desc: "This is a photo of my nephews Morgan and Toby who live in Arizona with my sister",
       likes: 0,
       visible: true,
     };
     welcome.photos[1] = {
-      pic: "images/imageTwo.jpg",
-      desc: "Photo description",
+      pic: "/images/image - 2.jpg",
+      desc: "This is the first selfie ever taken of my Fiance and I when we first met... We have taken a lot more since then",
       likes: 0,
       visible: true,
     };
     welcome.photos[2] = {
-      pic: "images/imageThree.jpg",
-      desc: "Photo description",
+      pic: "/images/image - 3.jpg",
+      desc: "My mom, Shes was/is still pretty awesome.  I wish you guys could have met her at the potluck",
       likes: 0,
       visible: true,
     };
     welcome.photos[3] = {
-      pic: "images/imageFour.jpg",
-      desc: "Photo description",
+      pic: "/images/image - 4.jpg",
+      desc: "My mom and my best friend surprised me with a birthday party last year, it was kinda awesome",
       likes: 0,
       visible: true,
     };
     welcome.photos[4] = {
-      pic: "images/imageFive.jpg",
-      desc: "Photo description",
+      pic: "/images/image - 5.jpg",
+      desc: "I took a trip with my best friend to San Fran. It was a blast even though we got lost in Muir Woods",
       likes: 0,
       visible: true,
     };
     welcome.photos[5] = {
-      pic: "images/imageSix.jpg",
-      desc: "Photo description",
+      pic: "/images/image - 6.jpg",
+      desc: "This is a photo from the night I proposed to Shari, She didnt know I made this face and now its on your website.",
       likes: 0,
       visible: true,
     };
